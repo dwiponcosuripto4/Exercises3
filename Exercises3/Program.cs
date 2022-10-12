79cb67859b30dff5d45951d7ede27b23019d064c
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2022-10-12 16:53:40 +0700

    20210140078

--- Exercises3/Program.cs
+++ Exercises3/Program.cs
@@ -4,7 +4,7 @@ namespace Exercises3
 {
     class Program
     {
-        private int[] a = new int[1890];
+        private int[] a = new int[98];
         private int n;
         public void read()
         {
@@ -13,7 +13,7 @@ namespace Exercises3
                 Console.Write("Masukan banyaknya elemen pada Array: ");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
-                if (n <= 1890)
+                if (n <= 98)
                     break;
                 else
                     Console.WriteLine("\nArray dapat mempunyai maksimal 1890 elemen. \n");
@@ -25,12 +25,47 @@ namespace Exercises3
 
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("");
-                Console.WriteLine("----------------------------------");
-                Console.WriteLine(" Elemen array yang telah tersusun ");
-                Console.WriteLine("----------------------------------");
-                for (int ds = n-2; ds )
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
             }
+        }
+        public void display()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine(" Elemen array yang telah tersusun ");
+            Console.WriteLine("----------------------------------");
+            for (int ds = n - 2; ds < n; ds++)
+            {
+                Console.WriteLine(a[n - 1]);
+            }
+            Console.WriteLine("");
+        }
+        public void DwiponcoArray()
+        {
+            for (int i = 1; i < n; i++)
+            {
+                for (int ds = n-2; ds < n - i; ds++)
+                {
+                    if (a[ds] > a[n - 1])
+                    {
+                        int temp;
+                        temp = a[ds];
+                        a[ds] = a[n - 1];
+                        a[n - 1] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program mylist = new Program();
+            mylist.read();
+            mylist.DwiponcoArray();
+            mylist.display();
+            Console.WriteLine("\n\nTekan Tombol Apa Saja Untuk Keluar.");
+            Console.Read();
         }
     }
 }
